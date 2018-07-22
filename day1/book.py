b5d86f2cffbe171def6ba74c834a4223e4117470
wxueyanghj@163.com <wxueyanghj@163.com>
2018-07-22 10:00:33 +0800

    zui jin xue xi geng xin

--- day1/book.py
+++ day1/book.py
@@ -6,3 +6,24 @@
 # @File : book.py 
 # @Software: PyCharm
 print("hello,world");
+
+a = int(input("请输入一个整数"));
+if(20<a<30):
+    print("true")
+else:
+    print("false")
+
+print(1 // 2);
+
+'''
+这是多行注释，使用单引号。
+这是多行注释，使用单引号。
+这是多行注释，使用单引号。
+'''
+
+
+"""
+这是多行注释，使用双引号。
+这是多行注释，使用双引号。
+这是多行注释，使用双引号。
+"""
---
+++ day1/chinese.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2018/7/22 上午12:05 
+# @Author : wangxueyang[wxueyanghj@163.com]
+# @Site :  
+# @File : chinese.py 
+# @Software: PyCharm
+
+'''
+
+'''
