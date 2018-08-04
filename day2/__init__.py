b6cd81b092160e2670aca2ca6b37cb1fe7501466
wxueyanghj@163.com <wxueyanghj@163.com>
2018-08-04 10:52:49 +0800

    同步文件

---
+++ day2/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2018/7/31 下午1:54 
+# @Author : wangxueyang[wxueyanghj@163.com]
+# @Site :  
+# @File : __init__.py.py 
+# @Software: PyCharm
+
+print("test file")
